54c8d5a436ccf48e62792c8621b773df77c958ab
dughetti <dughetti@zesmind.com>
2015-06-24 15:51:31 -0300

    Fix message undefined

--- api/responses/notFound.js
+++ api/responses/notFound.js
@@ -25,15 +25,7 @@ module.exports = function notFound (data, err) {
     var res = this.res;
     var sails = req._sails;
     var d = new Date();
-    var response = '';
-
-    if (req.options.action) {
-        response = util.format('[%s] %s %s - %s %s %d - ', d.toLocaleString(), req.options.controller, req.options.action, req.method, req.url, 404);
-    }
-    else {
-        response = util.format('[%s] - %s %s %d - ', d.toLocaleString(), req.method, req.url, 404);
-    }
-
+    var response = util.format('[%s] - %s %s %d - ', d.toLocaleString(), req.method, req.url, 404)
 
     // Set status code
     res.status(404);
